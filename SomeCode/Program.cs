dce5c7b78498f041bcd82c604cc7396e9508dbfb
FrogHus <FrogHus@DESKTOP-G345A7B>
2024-03-17 16:27:00 +0100

    Add project files.

---
+++ SomeCode/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть розмiрнiсть матрицi (рядки та стовпцi):");
+        Console.Write("Рядки: ");
+        int rows = int.Parse(Console.ReadLine());
+        Console.Write("Стовпцi: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        int[,] matrix = GenerateRandomMatrix(rows, cols);
+        PrintMatrix(matrix);
+    }
+
+    static int[,] GenerateRandomMatrix(int rows, int cols)
+    {
+        Random random = new Random();
+        int[,] matrix = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = random.Next(100);
+            }
+        }
+        return matrix;
+    }
+
+    static void PrintMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        Console.WriteLine("Результат:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
